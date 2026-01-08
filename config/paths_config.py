39f5bb601b2aea7fa741977edaf3239197f29df5
scorned <scornedcode@gmail.com>
2026-01-08 15:26:01 +0100

    change Paths config because the files have changed

--- config/paths_config.py
+++ config/paths_config.py
@@ -1,9 +1,10 @@
 class MinioConfig:
-    
     BUCKET_RAW_DATA = "sakura-bloom-data"
     BUCKET_GEO_DATA = "geo-data"
     BUCKET_WEATHER_DATA = "weather-data"
 
-    RAW_BLOOM_DATA_FILE = "kaggle/sakura_full_bloom_dates.csv"
+    KAGGLE_PREFIX = "kaggle"
 
+    RAW_BLOOM_DATA_FILE = f"{KAGGLE_PREFIX}/full_bloom_2010_2025.csv"
+    DATASET_URI = f"s3://{BUCKET_RAW_DATA}/{KAGGLE_PREFIX}"
     GEO_NOMINATIM_PATH = "nominatim/{city}/{timestamp}.json"
